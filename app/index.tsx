f91ca6cca978527f8deb105d8cd5d8ba73a8d85c
mustafadurmaz <mdmz_001@hotmail.com>
2024-08-11 00:25:31 +0300

    Text, View, TextInput, Pressable used

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,36 @@
 import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+
   return (
     <View style={styles.container}>
       <Text>Name</Text>
-      <TextInput 
-      placeholder="Enter your name"
-      style={styles.textInputStyle}
-      keyboardType="default"
+      <TextInput
+        placeholder="Enter your name"
+        style={styles.textInputStyle}
+        onChangeText={setName}
+        value={name}
       />
       <Text>Surname</Text>
       <TextInput
-      placeholder="Enter your surname"
-      style={styles.textInputStyle} />
+        placeholder="Enter your surname"
+        style={styles.textInputStyle}
+        onChangeText={setSurname}
+        value={surname}
+      />
+
+      <Pressable
+        style={({ pressed }) => [
+          { backgroundColor: pressed ? "gray" : "blue" },
+          styles.button,
+        ]}
+        onPress={() => alert(`Hello ${name} ${surname}`)}
+      >
+        <Text style={styles.buttonText}>Submit</Text>
+      </Pressable>
     </View>
   );
 }
@@ -31,5 +49,16 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginVertical: 10,
     textAlign: "center",
+  },
+  button: {
+    width: "80%",
+    height: 50,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  buttonText: {
+    fontWeight: "bold",
+    color: "white",
   },
 });
