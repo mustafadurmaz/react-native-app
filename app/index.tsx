7be0f7a474efaab74bf4e0e93a7e1be428c6e484
mustafadurmaz <mdmz_001@hotmail.com>
2024-08-24 13:37:02 +0300

    redux toolkit installed

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import RootNavigation from "@/src/navigation/RootNavigation";
-
+import { Provider } from "react-redux";
+import { store } from "@/src/redux/store";
 
 const App = () => {
   return (
-    <RootNavigation />
+    <Provider store={store}>
+      <RootNavigation />
+    </Provider>
   );
 };
 
