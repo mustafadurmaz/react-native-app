4417d65f683ec741798745a881c9973199f25fa7
mustafadurmaz <mdmz_001@hotmail.com>
2024-08-27 22:37:44 +0300

    firebase env edit

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,16 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from '@env';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA5huvL-6vZnzdtKrofJdDk0jeT-NkxXxA",
-  authDomain: "mobile-app-5f340.firebaseapp.com",
-  projectId: "mobile-app-5f340",
-  storageBucket: "mobile-app-5f340.appspot.com",
-  messagingSenderId: "799055796768",
-  appId: "1:799055796768:web:2cbd82b707ac513d3a3a45"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID
 };
 
 // Initialize Firebase
