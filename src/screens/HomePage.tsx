af14aeb1b8b6d0d2345061b88cfe7daf7cc5a936
mustafadurmaz <mdmz_001@hotmail.com>
2024-09-06 08:01:27 +0300

    get data from firebase store

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -1,10 +1,20 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { collection, addDoc } from "firebase/firestore";
+import React, { useState } from "react";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 import { db } from "../../firebaseConfig";
 import { CustomButton } from "../components";
 
 const HomePage = () => {
+  const [data, setData] = useState<any>([]);
+  console.log("data", data);
+
+  // Firestore'a veri gönderme işlemi
   const sendData = async () => {
     try {
       const docRef = await addDoc(collection(db, "reactNativeLesson"), {
@@ -16,6 +26,18 @@ const HomePage = () => {
       console.error("Error adding document: ", e);
     }
   };
+
+  // Firestore'dan veri çekme işlemi
+
+  const getData = async () => {
+    const querySnapshot = await getDocs(collection(db, "reactNativeLesson"));
+    querySnapshot.forEach((doc) => {
+      // console.log(`${doc.id} => ${doc.data()}`);
+      setData([...data, doc.data()]);
+    });
+  };
+
+ 
   return (
     <View style={styles.container}>
       <Text>HomePage</Text>
@@ -25,6 +47,13 @@ const HomePage = () => {
         buttonColor="blue"
         pressetButtonColor="gray"
         handleOnPress={sendData}
+      />
+      <CustomButton
+        title={"Get Data"}
+        setWidth={"40%"}
+        buttonColor="blue"
+        pressetButtonColor="gray"
+        handleOnPress={getData}
       />
     </View>
   );
